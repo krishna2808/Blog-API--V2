6184e7239973d8dcf2d78211de0c1b010cfe73cd
= <=>
2024-05-12 17:10:17 +0530

    the change files terraform configuration

--- terraform/project_setup_script_without_docker_container.sh
+++ terraform/project_setup_script_without_docker_container.sh
@@ -9,6 +9,7 @@ sudo chmod 777 /etc/nginx/sites-available/blog_project.conf
 
 sudo mkdir -p /app
 sudo git clone https://github.com/krishna2808/Blog-API--V2.git /app/blog_project
+
 sudo chmod -R 777 /app/*
 sudo cat /app/blog_project/nginx/nginx.conf > /etc/nginx/sites-available/blog_project.conf
 sudo ln -s /etc/nginx/sites-available/blog_project.conf  /etc/nginx/sites-enabled/
@@ -21,12 +22,13 @@ sudo apt-get update -y
 # sudo apt-get install python3.10 -y
 sudo apt-get install python3.10-dev -y
 sudo apt-get install python3.10-venv -y
-sudo apt-get install python3-pip  -y
+sudo apt-get install python3-pip -y
 
 # project setup in ec2 instance 
 sudo apt-get install libmysqlclient-dev -y
+sudo apt-get install pkg-config -y
 cd /app/blog_project/
-python3.10 -m venv venv
+sudo python3.10 -m venv venv
 sudo chmod -R 777 venv/*
 source venv/bin/activate
 cd /app/blog_project/blog/
