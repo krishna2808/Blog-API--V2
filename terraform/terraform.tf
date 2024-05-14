88aecbc3d3f7c8038f447cf049d8a199fbf1d340
= <=>
2024-05-14 07:28:01 +0530

    changing settings.py

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -21,6 +21,8 @@ provider "aws" {
 # To create VPC 
 resource "aws_vpc" "custom_vpc_tf" {
   cidr_block = "192.168.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
   tags = {
     Name = "custom_vpc_tf"
   }
