4e64b52f3f3ed578e7e078ada056080e933393ef
Krishna kushvansi <68548987+krishna2808@users.noreply.github.com>
2024-05-27 22:06:15 +0530

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    sqlite
+    # sqlite
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
