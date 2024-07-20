f46e74921b467d9e2a04f065a0796606f4ca6381
= <=>
2024-07-20 14:07:30 +0530

    changing in some files

--- frontend/src/components/chat/ChatApp.jsx
+++ frontend/src/components/chat/ChatApp.jsx
@@ -3,6 +3,8 @@ import '../../assets/styles/chat.css';
 
 import UserChatList from './UserChatList';
 import UserChat from './UserChat';
+import Header from '../common/Header';
+
 
 function App() {
   const [selectedUser, setSelectedUser] = useState(null);
@@ -12,14 +14,23 @@ function App() {
   };
 
   return (
-    <div className="app-container">
-      <div className="user-chat-list">
-        <UserChatList onSelect={handleUserSelect} />
-      </div>
-      <div className="user-chat">
-        {selectedUser ? <UserChat user={selectedUser} /> : <div className='select-chat'>Select a chat to start messaging</div>}
-      </div>
-    </div>
+    <>
+    
+        <Header />
+    
+        <div className="app-container">
+        <div className="user-chat-list">
+            <UserChatList onSelect={handleUserSelect} />
+        </div>
+        <div className="user-chat">
+            {selectedUser ? <UserChat user={selectedUser} /> : <div className='select-chat'>Select a chat to start messaging</div>}
+        </div>
+        </div>
+
+
+
+    </>
+   
   );
 }
 
